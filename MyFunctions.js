0ae0230c71af8a9ee8336a328943ad796148e8ba
Jeff Allaire <jeff.allaire@gmail.com>
2017-07-17 15:43:41 -0500

    Javascript and DOM event listener project

--- MyFunctions.js
+++ MyFunctions.js
@@ -1,2 +1,17 @@
-document.getElementById("demo").innerHTML = "My First JavaScript";
+//Event Listeners
+
+//Create DIV element
+var div = document.createElement('DIV');
+//Set height of element
+div.style.height = "100vh";
+//Append Element to DOM
+document.body.appendChild(div);
+//Add Event listener to Element
+div.addEventListener('mousemove', function(event)  {
+  console.log(event);
+  var x = event.clientX;
+  var y = event.clientY;
+  div.textContent = x + ', ' + y;
+  div.style.backgroundColor = 'rgb(' +x+ ','+y+ ', 50)';
+});
 
