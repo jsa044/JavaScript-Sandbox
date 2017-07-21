b1b2a54761282336a5322dbf202a2157899f6677
Jeff Allaire <admin@allairewebdevelopmentllc.com>
2017-07-20 19:28:12 -0500

    Update header.php
    Add meta info and modified the PHP date function contents

--- header.php
+++ header.php
@@ -1,12 +1,15 @@
-<?php echo date("Y") ?>
+<?php echo date("Y/m/d")  ?>
 
 <!DOCTYPE html>
 
-<html>
-	
+<html lang="en-US">	
 	<head>
 		<title>JSandbox</title>
-		
+		<meta charset="UTF-8"> 
+		<meta name="description" content="Javascript Events">
+		<meta name="keywords" content="JavaScript HTML,CSS,XML">
+		<meta name="author" content="Jeff Allaire">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	</head>
 	
 	<body id="bg-color">
